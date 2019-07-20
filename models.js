483af1f7490537b736083ffc59084d3f06b3cb4e
RH Blanchfield <dev@Alamedas-MacBook-Air.local>
2019-07-20 15:31:36 -0400

    models

---
+++ models.js
@@ -0,0 +1,46 @@
+const Sequelize = require('sequelize');
+
+
+//Sequelize
+
+const sequelize = new Sequelize({
+  database: 'barrelaged_db ',
+  dialect: 'postgres',
+  define: {
+    underscored: true,
+  },
+});
+
+
+//USER MODEL
+
+const User = sequelize.define('users', {
+  name: Sequelize.STRING,
+  email: Sequelize.STRING,
+  password_digest: Sequelize.STRING,
+});
+
+
+//WHISKEY MODEL
+const Whiskey = sequelize.define('whiskey', {
+
+  name: Sequelize.STRING,
+  type: Sequelize.STRING,
+  agg: Sequelize.FLOAT,
+});
+
+
+//REVIEW
+const Review = sequelize.define('review', {
+
+  score: Sequelize.INTEGER,
+  review: Sequelize.STRING,
+
+});
+
+module.exports = {
+  sequelize,
+  User,
+  Whiskey,
+  Review,
+};
