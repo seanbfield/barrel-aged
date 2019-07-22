f57c37d380b8aaa6bf08a6300c5d8fd4c67546b5
Misha Kessler <misha.kessler@gmail.com>
2019-07-22 11:13:02 -0400

    Add components, React structure.

---
+++ client/src/pages/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class User extends React.Component {
+  render() {
+    return (
+      <main>
+        <p>This is the user page</p>
+      </main>
+    )
+  }
+}
+
+export default User;
