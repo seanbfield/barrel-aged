bd258fa7ee70c2bc352ecd923f37eec1854328a4
Brian Weitz <brianweitz@Brians-MacBook-Pro-2.local>
2019-07-23 11:31:32 -0400

    basic agegate setup

--- client/src/pages/AgeGate.jsx
+++ client/src/pages/AgeGate.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class AgeGate extends React.Component {
-  render() {
-    return (
-      <div>
-        <form>
-          <label htmlFor="date">Please enter your birthdate:</label>
-          <input type="date" id="date" />
-        </form>
-        <Link to='/home'>Enter Site</Link>
-      </div>
-    )
-  }
+const AgeGate = () => {
+  return (
+    <div>
+      <h2>By clicking here, I certify that I am at least 21 years of age.</h2>
+      <Link to='/home'>Enter Site</Link>
+    </div>
+  )
 }
 
 export default AgeGate;
