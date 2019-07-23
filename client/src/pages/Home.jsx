2c8468fe157a541641f72794218d11dff64cadc3
RH Blanchfield <dev@Alamedas-MacBook-Air.local>
2019-07-23 18:51:17 -0400

    user registration

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,43 +1,89 @@
 import React from 'react';
 import { Route, Link, withRouter } from 'react-router-dom';
+import { ping, userSignup } from '../services/api-helper'
+import Header from '../components/Header'
+import axios from 'axios'
 
 import RegisterForm from '../components/RegisterForm';
 import LoginForm from '../components/LoginForm';
 import ReviewForm from '../components/ReviewForm';
 
-import AgeGate from './AgeGate'
-import Landing from './Landing'
+import AgeGate from './AgeGate';
+import Landing from './Landing';
 import User from './User';
 import Whiskey from './Whiskey';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      register: false,
-      login: false
+      currentView: 'login',
+      loginFormData: {
+        user: '',
+        name: '',
+        password: '',
+      },
+      registerFormData: {
+        user: '',
+        email: '',
+        password: '',
+      }
     }
   }
-  handleRegister = () => {
-    this.setState({
-      register: true,
-      login: false
-    })
-  }
-  handleLogin = () => {
-    this.setState({
-      login: true,
-      register: false
-    })
+
+
+  // SB - Handle Register Form Change
+  handleRegisterFormChange = (ev) => {
+    const { name, value } = ev.target;
+    this.setState(prevState => ({
+      registerFormData: {
+        ...prevState.registerFormData,
+        [name]: value
+      }
+    }));
   }
 
-  goToUser = () => {
-    this.props.history.push('/user')
+  // SB - Registration Submit
+
+  handleRegisterSubmit = async (ev) => {
+    try {
+      ev.preventDefault();
+      const user = await userSignup(this.state.registerFormData);
+      console.log(user);
+      this.setState({
+        registerForm: {
+          user: '',
+          password: '',
+          email: ''
+        },
+        currentUser: user,
+        currentView: 'landing'
+      });
+      this.props.history.push('/landing')
+    } catch (e) {
+      console.log(e)
+    }
   }
+
+  async componentDidMount() {
+    const data = await axios.get('http://localhost:3000/users');
+    console.log(data);
+
+  }
+
   render() {
     return (
       <div>
-        <LoginForm />
+        <Header />
+        <>
+          <RegisterForm
+            registerForm={this.state.registerFormData}
+            handleSubmit={this.handleRegisterSubmit}
+            handleChange={this.handleRegisterFormChange}
+          />
+        </>
+
       </div>
     )
   }
