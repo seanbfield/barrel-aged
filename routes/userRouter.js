3297354dd3797de25594119091bc05bd69761e4b
Misha Kessler <misha.kessler@gmail.com>
2019-07-24 06:57:16 -0400

    Add error handling to server.

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -9,7 +9,6 @@ const userRouter = Router();
 // Create User (Signup) – BW
 
 userRouter.post('/signup', async (req, res) => {
-  debugger;
   const { user, password, email } = req.body;
   const pwDigest = await bcrypt.hash(password, SALT);
   const newUser = await User.create({
@@ -46,97 +45,129 @@ userRouter.post('/login', async (req, res) => {
 // Index Users – BW
 
 userRouter.get('/', async (req, res) => {
-  const users = await User.findAll();
-  res.json(users);
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (e) {
+    next(e);
+  }
 });
 
 // Show User – SB
 
 userRouter.get('/:id', async (req, res) => {
-  const specificUser = await User.findByPk(req.params.id);
-  res.json(specificUser);
+  try {
+    const specificUser = await User.findByPk(req.params.id);
+    res.json(specificUser);
+  } catch (e) {
+    next(e);
+  }
 });
 
 // Update User – MK
 
 userRouter.put('/:id', async (req, res) => {
-  const { id } = req.params;
-  await User.update({
-    first_name: req.body.first_name,
-    user_name: req.body.user_name,
-    email: req.body.email,
-    location: req.body.location,
-    fav_whiskey: req.body.fav_whiskey,
-  }, {
-      where: {
-        id,
-      },
-    });
-  const updateUser = await User.findByPk(id);
-  res.json(updateUser);
+  try {
+    const { id } = req.params;
+    await User.update({
+      first_name: req.body.first_name,
+      user_name: req.body.user_name,
+      email: req.body.email,
+      location: req.body.location,
+      fav_whiskey: req.body.fav_whiskey,
+    }, {
+        where: {
+          id,
+        },
+      });
+    const updateUser = await User.findByPk(id);
+    res.json(updateUser);
+  } catch (e) {
+    next(e);
+  }
 });
 
 // Create Review - BW
 
 userRouter.post('/:user_id/whiskey/:id/review', async (req, res) => {
-  const user = await User.findByPk(req.params.user_id);
-  const whiskey = await Whiskey.findByPk(req.params.id);
-  const review = await Review.create(req.body);
-  review.setUser(user);
-  review.setWhiskey(whiskey);
-  res.json(review);
+  try {
+    const user = await User.findByPk(req.params.user_id);
+    const whiskey = await Whiskey.findByPk(req.params.id);
+    const review = await Review.create(req.body);
+    review.setUser(user);
+    review.setWhiskey(whiskey);
+    res.json(review);
+  } catch (e) {
+    next(e);
+  }
 });
 
 // Index Reviews - SB
 
 userRouter.get('/review', async (req, res) => {
-  const everyReview = await Review.findAll();
-  console.log(everyReview);
-  res.json(everyReview);
+  try {
+    const everyReview = await Review.findAll();
+    console.log(everyReview);
+    res.json(everyReview);
+  } catch (e) {
+    next(e);
+  }
 });
 
 // Index User's Reviews - SB
 
 userRouter.get('/:id/review', async (req, res) => {
-  const { id } = req.params;
-  const findReview = await Review.findAll({
-    where: { id },
-    include: [{ model: User }],
-  });
-  res.json(findReview);
+  try {
+    const { id } = req.params;
+    const findReview = await Review.findAll({
+      where: { id },
+      include: [{ model: User }],
+    });
+    res.json(findReview);
+  } catch (e) {
+    next(e);
+  }
 });
 
 // Update Review - SB
 
 userRouter.put('/:user_id/review/:id', async (req, res) => {
-  const { id } = req.params;
-  await Review.update({
-    rating: req.body.rating,
-    comment: req.body.comment,
-  }, {
-      where: {
-        id,
-      },
-    });
-  const editReview = await Review.findByPk(id);
-  res.json(editReview);
+  try {
+    const { id } = req.params;
+    await Review.update({
+      rating: req.body.rating,
+      comment: req.body.comment,
+    }, {
+        where: {
+          id,
+        },
+      });
+    const editReview = await Review.findByPk(id);
+    res.json(editReview);
+  } catch (e) {
+    next(e);
+  }
 });
 
 // Delete Review - BW
 
 userRouter.delete('/:user_id/review/:id', async (req, res) => {
-  const user = await User.findByPk(req.params.user_id);
-  const review = await Review.findByPk(req.params.id);
-  if (
-    user.id === review.userId) {
-    await Review.destroy({
-      where: {
-        id: review.id,
-      },
-    });
-    res.json('Review deleted');
-  } else {
-    res.status(401).send('Not Authorized');
+  try {
+    const user = await User.findByPk(req.params.user_id);
+    const review = await Review.findByPk(req.params.id);
+    if (
+      user.id === review.userId) {
+      await Review.destroy({
+        where: {
+          id: review.id,
+        },
+      });
+      res.json('Review deleted');
+    } else {
+      res.status(401).send('Not Authorized');
+    }
+  } catch (e) {
+    next(e);
   }
 });
 
--- routes/whiskeyRouter.js
+++ routes/whiskeyRouter.js
@@ -7,29 +7,41 @@ const whiskeyRouter = Router();
 // Create Whiskey – NB
 
 whiskeyRouter.post('/', async (req, res) => {
-  const data = req.body;
-  const newWhiskey = await Whiskey.create(data);
-  res.json(newWhiskey);
+  try {
+    const data = req.body;
+    const newWhiskey = await Whiskey.create(data);
+    res.json(newWhiskey);
+  } catch (e) {
+    next(e);
+  }
 });
 
 // Index Whiskeys – NB
 
 whiskeyRouter.get('/', async (req, res) => {
-  const AllWhiskey = await Whiskey.findAll({
-    include: [Review],
-  });
-  res.json(AllWhiskey);
+  try {
+    const AllWhiskey = await Whiskey.findAll({
+      include: [Review],
+    });
+    res.json(AllWhiskey);
+  } catch (e) {
+    next(e);
+  }
 });
 
 // Index Whiskey's Reviews - MK
 
 whiskeyRouter.get('/:id/review', async (req, res) => {
-  const { id } = req.params;
-  const findReviews = await Whiskey.findAll({
-    where: { id },
-    include: [{ model: Review }],
-  });
-  res.json(findReviews);
+  try {
+    const { id } = req.params;
+    const findReviews = await Whiskey.findAll({
+      where: { id },
+      include: [{ model: Review }],
+    });
+    res.json(findReviews);
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = {
