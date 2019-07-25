2d5c9d64b1fb66ed1e0b1f1f55ff81edb6f1797c
RH Blanchfield <dev@Alamedas-MacBook-Air.local>
2019-07-25 08:30:37 -0400

    age gate video

---
+++ client/src/components/Slider.jsx
@@ -0,0 +1,30 @@
+// Inpired by: https://codepen.io/popmotion/pen/xWrbNm?editors=0010
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { useSpring, animated, interpolate } from 'react-spring'
+import { useGesture } from 'react-with-gesture'
+import '../App.css'
+
+function Slider({ children }) {
+  const [bind, { delta, down }] = useGesture()
+  const { x, bg, size } = useSpring({
+    x: down ? delta[0] : 0,
+    bg: `linear-gradient(120deg, ${delta[0] < 0 ? '#f093fb 0%, #f5576c' : '#96fbc4 0%, #f9f586'} 100%)`,
+    size: down ? 1.1 : 1,
+    immediate: name => down && name === 'x'
+  })
+  const avSize = x.interpolate({ map: Math.abs, range: [50, 300], output: ['scale(0.5)', 'scale(1)'], extrapolate: 'clamp' })
+  return (
+    <animated.div {...bind()} class="item" style={{ background: bg }}>
+      <animated.div class="av" style={{ transform: avSize, justifySelf: delta[0] < 0 ? 'end' : 'start' }} />
+      <animated.div class="fg" style={{ transform: interpolate([x, size], (x, s) => `translate3d(${x}px,0,0) scale(${s})`) }}>
+        {children}
+      </animated.div>
+    </animated.div>
+  )
+}
+
+ReactDOM.render(<Slider>Enter.</Slider>, document.getElementById('root'))
+
+export default Slider
--- client/src/pages/AgeGate.jsx
+++ client/src/pages/AgeGate.jsx
@@ -1,44 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import glass from '../assets/images/glass.png'
+import Slider from '../components/Slider'
 import logo from '../assets/images/ba-logo.png'
 
+
 class AgeGate extends React.Component {
-  state = {
-    resizeNotifier: () => { },
-  }
-
-
   render() {
-    // const videoOptions = {
-    //   src: '',
-    //   autoPlay: true,
-    //   loop: true,
-    //   muted: true
-    // };
-    // const style = {
-    //   width: '150vw',
-    //   height: '150vh',
-    //   position: 'fixed',
-    //   margin: 'auto',
-    //   top: '-25vh',
-    //   left: '-25vw',
-    //   zIndex: -2,
-    // };
-
-
     return (
-
-
-      <div className="App">
-        <header className="App-header">
-
-          <img src={logo} width="40%" className="App-logo" alt="logo" />
-          <div>
-            <h2>By clicking here, I certify that I am at least 21 years of age.</h2>
-            <Link to='/home'>Enter Site</Link>
-          </div>
-        </header>
+      <div className="auth-body">
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} width="80%" className="App-logo" alt="logo" />
+            <div className="age-gate-box">
+              <h1 className="age-gate-h1">By swiping right, I certify that I am at least 21 years of age.</h1>
+              {/* <Link to='/home'>Enter Site</Link> */}
+            </div>
+            <Slider />
+          </header>
+        </div>
       </div>
     )
   }
