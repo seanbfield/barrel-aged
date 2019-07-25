60715087c460cd6172119b8fbfaf94d61e3083d1
Misha Kessler <misha.kessler@gmail.com>
2019-07-24 21:14:23 -0400

    Revert "Add updated assets; update logo import to match."
    This reverts commit bc1574a8cc19d9c26d3596eca08429b3123bb85c.

--- client/src/pages/AgeGate.jsx
+++ client/src/pages/AgeGate.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/images/wordmark-light.png'
+import glass from '../assets/images/glass.png'
+import logo from '../assets/images/ba-logo.png'
 
 class AgeGate extends React.Component {
   state = {
